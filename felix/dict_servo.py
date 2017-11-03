14b69f9823370ba5a648b61cfa6b5246e93b6ac0
michelasmus <sumsalehcim@gmail.com>
2017-11-03 17:14:11 +0100

    update dict_servo
    deleted unnecessary listings

--- felix/dict_servo.py
+++ felix/dict_servo.py
@@ -13,57 +13,37 @@
 # TODO:
 # a) position-max and -min still in ticks
 
-
-servo1={
-    "ID"                : 1,
+servo_data=[
+{"ID"                : 1,
     "BAUDRATE"          : 1000000,
     "POSITION_MINIMUM"  : -76000,
     "POSITION_MAXIMUM"  : 76000,
-    "CLOCKWISE"         : 1,
-    "SPEED_MAXIMUM"     : 10000,
-    "DH_Theta"          : 0,
-    "DH_d"              : 0,
-    "DH_a"              : 0,
-    "DH_Alpha"          : 0
+    "CLOCKWISE"         : 1
 }
-
-servo2={
+,
+{
     "ID"                : 2,
     "BAUDRATE"          : 1000000,
     "POSITION_MINIMUM"  : -76000,
     "POSITION_MAXIMUM"  : 76000,
-    "CLOCKWISE"         : 1,
-    "SPEED_MAXIMUM"     : 10000,
-    "DH_Theta"          : 0,
-    "DH_d"              : 0,
-    "DH_a"              : 0,
-    "DH_Alpha"          : 0
+    "CLOCKWISE"         : 1
 }
-
-servo3={
+,
+{
     "ID"                : 3,
     "BAUDRATE"          : 1000000,
     "POSITION_MINIMUM"  : -45000,
     "POSITION_MAXIMUM"  : 76000,
-    "CLOCKWISE"         : 1,
-    "SPEED_MAXIMUM"     : 10000,
-    "DH_Theta": 0,
-    "DH_d": 0,
-    "DH_a": 0,
-    "DH_Alpha": 0
+    "CLOCKWISE"         : 1
 }
-
-servo4={
+,
+{
     "ID"                : 4,
     "BAUDRATE"          : 1000000,
     "POSITION_MINIMUM"  : -76000,
     "POSITION_MAXIMUM"  : 76000,
     "CLOCKWISE"         : 1,
-    "SPEED_MAXIMUM"     : 10000,
-    "DH_Theta": 0,
-    "DH_d": 0,
-    "DH_a": 0,
-    "DH_Alpha": 0
+    "SPEED_MAXIMUM"     : 10000
 }
+]
 
-servo_all=[servo1,servo2,servo3,servo4]
