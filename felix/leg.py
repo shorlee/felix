4289fd697dd5d5e2d66a1745227bdfdbf42405b7
michelasmus <sumsalehcim@gmail.com>
2017-11-24 11:37:01 +0100

    changed kinematic

--- felix/leg.py
+++ felix/leg.py
@@ -14,6 +14,7 @@
 
 import os
 import time
+import numpy as np
 
 try:
     from servo import servo
@@ -61,8 +62,10 @@ class leg:
     # =======================================
 
     # (analytical) forward kinematics with angles given in radians (!)
-    def forwardkin_alpha2end(self, alpha, beta, gamma, delta):
-        d0 = self.leg_data["d0"], a2 = self.leg_data["a2"], a3 = self.leg_data["a3"]
+    def forwardkin_alpha2end(self, alpha, gamma, beta, delta): # caution: changed beta and gamma!
+        d0 = self.leg_data["d0"]
+        a2 = self.leg_data["a2"]
+        a3 = self.leg_data["a3"]
         pos = [0, 0, 0, 1]
         pos[0] = ( a3 * np.cos(delta) * ( np.cos(alpha) * np.cos(beta) * np.cos(gamma) + np.sin(alpha) * np.sin(gamma) ) ) + ( a3 * np.sin(delta) * ( -np.cos(alpha) * np.cos(beta) * np.sin(gamma) + np.sin(alpha) * np.cos(gamma) ) ) + ( a2 * ( np.cos(alpha) * np.cos(beta) * np.cos(gamma) + np.sin(alpha) * np.sin(gamma) ) )
         pos[1] = ( a3 * np.cos(delta) * ( np.sin(alpha) * np.cos(beta) * np.cos(gamma) - np.cos(alpha) * np.sin(gamma) ) ) + ( a3 * np.sin(delta) * ( -np.sin(alpha) * np.cos(beta) * np.sin(gamma) - np.cos(alpha) * np.cos(gamma) ) ) + ( a2 * ( np.sin(alpha) * np.cos(beta) * np.cos(gamma) - np.cos(alpha) * np.sin(gamma) ) )
